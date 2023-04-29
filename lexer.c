843e759f0b1fa9c4951c639e085e2cf0705f52bf
victorG <victorgaonewlife@gmail.com>
2023-04-29 21:54:28 +1000

    Lexer: add binary token

--- lexer.c
+++ lexer.c
@@ -477,10 +477,39 @@ struct token *token_make_special_number_hexadecimal()
     return token_make_number_for_value(number);
 }
 
+void  lexer_validate_binary_string(const char* str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i ++)
+    {
+        if (str[i] != '0' && str[i] != '1')
+        {
+            compiler_error(lex_process->compiler, "This is not a valid binary number\n");
+        }
+    }
+}
+
+struct token* token_make_special_number_binary()
+{
+    // skip the 'b'
+    nextc();
+
+    unsigned long number = 0;
+    const char *number_str = read_number_str();
+    lexer_validate_binary_string(number_str);
+    number = strtol(number_str, 0, 2);
+    return token_make_number_for_value(number);
+}
+
 struct token *token_make_special_number()
 {
     struct token *token = NULL;
     struct token *last_token = lexer_last_token(); // pop off '0'
+
+    if (!last_token || !(last_token->type == TOKEN_TYPE_NUMBER && last_token->llnum == 0))
+    {
+        return token_make_identifier_or_keyword();  // x500, or b123
+    }
 
     lexer_pop_token();
 
@@ -488,6 +517,10 @@ struct token *token_make_special_number()
     if (c == 'x')
     {
         token = token_make_special_number_hexadecimal();
+    }
+    else if (c == 'b')
+    {
+        token = token_make_special_number_binary();
     }
 
     return token;
@@ -536,6 +569,8 @@ struct token *read_next_token()
     SYMBOL_CASE:
         token = token_make_symbol();
         break;
+
+    case 'b':
     case 'x':                                /* we have already tokenized `0` at */
         token = token_make_special_number(); /* this point, so we use case `x`   */
         break;
